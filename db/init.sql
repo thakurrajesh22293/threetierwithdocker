42797035f7e9612f0f952bc08e4e4cb1145cf265
thakurrajesh22293 <139438293+thakurrajesh22293@users.noreply.github.com>
2026-01-29 17:02:16 +0530

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE appdb;
+CREATE DATABASE IF NOT EXISTS appdb;
 
 USE appdb;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100)
 );
 
 INSERT INTO users (name) VALUES
-('Ateeq'),
+('Vishal'),
 ('Rajesh'),
 ('DevOps Engineer');
